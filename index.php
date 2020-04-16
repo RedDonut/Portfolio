4ad075bcdab8c8f8d6126d3c522d1643ed428fd7
Rohan Kanhai <57237481+RedDonut@users.noreply.github.com>
2020-04-15 17:38:29 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links Rohan Kanhai</p>'; ?> 
+    <ul>
+      <li><a href="index.html">Home</a></li></br>
+      <li><a href="Last_AboutMe.html">About Me</a></li></br>
+      <li><a href="Last_Projects.html">Projects</a></li></br>
+      <li><a href="Last_Resume.html">Resume</a></li></br>
+      <li><a href="Last_ContactMe.html">Contact Me</a></li></br>
+    </ul>
+  </body>
+</html>
